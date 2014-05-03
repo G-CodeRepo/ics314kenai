f411dcea6e5fc79e592249061136fb4a5e6e9f58
g-man64 <abut@hawaii.edu>
2014-05-02 23:08:51 -1000

    new 5/2/2014

--- ics314_VOR_abut/src/ics314_VOR_abut/VorReceiver.java
+++ ics314_VOR_abut/src/ics314_VOR_abut/VorReceiver.java
@@ -714,7 +714,8 @@ public class VorReceiver {
 			v.updateIncomingRadial(incomingRadial);
 			v.setOBS(90);
 			v.printVorStatus();
-			
+		
+			// end
 		} catch (IllegalArgumentException e) {
 			System.err.println("ERROR: OBS values must be within 1-360 degrees, where 360 can also be zero");
 		}
